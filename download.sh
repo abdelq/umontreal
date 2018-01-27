2ce7bf51ede6e595415f28dae652afcd3fd2e2bc
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-01-27 16:58:05 -0500

    Simplification of download script

--- download.sh
+++ download.sh
@@ -2,13 +2,15 @@
 
 USERNAME=""
 PASSWORD=""
-COURSEID=""
+COURSEID=
+
+IDENTIFICATION=https://identification.umontreal.ca
+STUDIUM=https://studium.umontreal.ca
 
 # Authentication
-HIDDEN_INPUTS=`curl -s https://identification.umontreal.ca/cas/login.aspx |
-    grep hidden`
-VIEWSTATE=`grep \"__VIEWSTATE\" <<< $HIDDEN_INPUTS | cut -d'"' -f 8`
-EVENTVALIDATION=`grep \"__EVENTVALIDATION\" <<< $HIDDEN_INPUTS | cut -d'"' -f 8`
+HIDDENINPUTS=`curl -s $IDENTIFICATION/cas/login.aspx | grep hidden`
+VIEWSTATE=`grep -oP 'VIEWSTATE" value="\K.*(?=")' <<< $HIDDENINPUTS`
+EVENTVALIDATION=`grep -oP 'EVENTVALIDATION" value="\K.*(?=")' <<< $HIDDENINPUTS`
 
 curl -s -o /dev/null -c /tmp/umontreal \
     --data-urlencode "txtIdentifiant=$USERNAME" \
@@ -16,27 +18,26 @@ curl -s -o /dev/null -c /tmp/umontreal \
     --data-urlencode "__VIEWSTATE=$VIEWSTATE" \
     --data-urlencode "__EVENTVALIDATION=$EVENTVALIDATION" \
     --data-urlencode "btnValider=Valider" \
-    https://identification.umontreal.ca/cas/login.aspx
+    $IDENTIFICATION/cas/login.aspx
 
 curl -s -o /dev/null -c /tmp/umontreal -b /tmp/umontreal \
-    -LG -d "service=https://studium.umontreal.ca/login/index.php" \
-    https://identification.umontreal.ca/cas/login.ashx
+    -LG -d "service=$STUDIUM/login/index.php" \
+    $IDENTIFICATION/cas/login.ashx
 
-SESSKEY=`curl -s -b /tmp/umontreal https://studium.umontreal.ca |
-    grep -oP "(?<=sesskey\":\")\w+"`
+SESSKEY=`curl -s -b /tmp/umontreal $STUDIUM | grep -om 1 -P 'sesskey=\K\w+'`
 
 # Students
 curl -b /tmp/umontreal -o students.csv -d "id=$COURSEID" \
-    https://studium.umontreal.ca/grade/export/txt/export.php
+    $STUDIUM/grade/export/txt/export.php
 
 # Homework
 HOMEWORK=`curl -s -b /tmp/umontreal -G -d "id=$COURSEID" \
-    https://studium.umontreal.ca/mod/assign/index.php |
+    $STUDIUM/mod/assign/index.php |
     grep "assign/view" | grep -i "$1" | tail -n1`
 
-HOMEWORK_ID=`grep -oP "(?<=id=)\d+" <<< $HOMEWORK`
-HOMEWORK_NAME=`grep -oP "(?<=>)\w+(?=<)" <<< $HOMEWORK`
+HOMEWORK_ID=`grep -oP "id=\K\d+" <<< $HOMEWORK`
+HOMEWORK_NAME=`grep -oP ">\K\w+(?=<)" <<< $HOMEWORK`
 
 curl -b /tmp/umontreal -o "$HOMEWORK_NAME.zip" \
     -G -d "id=$HOMEWORK_ID" -d "action=downloadall" \
-    https://studium.umontreal.ca/mod/assign/view.php
+    $STUDIUM/mod/assign/view.php
