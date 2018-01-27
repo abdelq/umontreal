d3de590280fa705cc60a3c3f8f157bd9efc38da8
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-01-26 23:16:04 -0500

    Small cleanup

--- download.sh
+++ download.sh
@@ -5,9 +5,10 @@ PASSWORD=""
 COURSEID=""
 
 # Authentication
-HIDDEN_INPUTS=`curl -s https://identification.umontreal.ca/cas/login.aspx | grep hidden`
-VIEWSTATE=`grep \"__VIEWSTATE\" <<< $HIDDEN_INPUTS | cut -d'"' -f 8` # XXX
-EVENTVALIDATION=`grep \"__EVENTVALIDATION\" <<< $HIDDEN_INPUTS | cut -d'"' -f 8` # XXX
+HIDDEN_INPUTS=`curl -s https://identification.umontreal.ca/cas/login.aspx |
+    grep hidden`
+VIEWSTATE=`grep \"__VIEWSTATE\" <<< $HIDDEN_INPUTS | cut -d'"' -f 8`
+EVENTVALIDATION=`grep \"__EVENTVALIDATION\" <<< $HIDDEN_INPUTS | cut -d'"' -f 8`
 
 curl -s -o /dev/null -c /tmp/umontreal \
     --data-urlencode "txtIdentifiant=$USERNAME" \
@@ -21,7 +22,8 @@ curl -s -o /dev/null -c /tmp/umontreal -b /tmp/umontreal \
     -LG -d "service=https://studium.umontreal.ca/login/index.php" \
     https://identification.umontreal.ca/cas/login.ashx
 
-SESSKEY=`curl -s -b /tmp/umontreal https://studium.umontreal.ca | grep -oP "(?<=sesskey\":\")\w+"` # XXX
+SESSKEY=`curl -s -b /tmp/umontreal https://studium.umontreal.ca |
+    grep -oP "(?<=sesskey\":\")\w+"`
 
 # Students
 curl -b /tmp/umontreal -o students.csv -d "id=$COURSEID" \
@@ -30,10 +32,10 @@ curl -b /tmp/umontreal -o students.csv -d "id=$COURSEID" \
 # Homework
 HOMEWORK=`curl -s -b /tmp/umontreal -G -d "id=$COURSEID" \
     https://studium.umontreal.ca/mod/assign/index.php |
-    grep "assign/view" | grep -i "$1" | tail -n1` # XXX
+    grep "assign/view" | grep -i "$1" | tail -n1`
 
-HOMEWORK_ID=`grep -oP "(?<=id=)\d+" <<< $HOMEWORK` # XXX
-HOMEWORK_NAME=`grep -oP "(?<=>)\w+(?=<)" <<< $HOMEWORK` # XXX
+HOMEWORK_ID=`grep -oP "(?<=id=)\d+" <<< $HOMEWORK`
+HOMEWORK_NAME=`grep -oP "(?<=>)\w+(?=<)" <<< $HOMEWORK`
 
 curl -b /tmp/umontreal -o "$HOMEWORK_NAME.zip" \
     -G -d "id=$HOMEWORK_ID" -d "action=downloadall" \
