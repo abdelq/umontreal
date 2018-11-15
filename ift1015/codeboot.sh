afa7ac3a49e4f3c31fbe7ad16d52333cf5c4776a
FredericHamel <frederic.hamel123@gmail.com>
2018-11-15 14:33:15 -0500

    Encode substitution @ to @@ in source file.

--- ift1015/codeboot.sh
+++ ift1015/codeboot.sh
@@ -2,5 +2,5 @@
 
 CODEBOOT="http://www-labs.iro.umontreal.ca/~codeboot/codeboot"
 
-FILE="@C$(basename "$1")@0$(sed ':a;N;$!ba;s/\r//g;s/\n/@N/g' "$1")@E"
+FILE="@C$(basename "$1")@0$(sed ':a;N;$!ba;s/@/@@/g;s/\r//g;s/\n/@N/g' "$1")@E"
 xdg-open "$CODEBOOT/query.cgi?REPLAY=$(base64 <<< $FILE)"
