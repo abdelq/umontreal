cbca4b063953fec7a68de6b750c56d34c5956598
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-01-20 13:54:50 -0500

    Slightly modified prepare script

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Télécharger la liste d'étudiants sous format CSV :
+https://studium.umontreal.ca/grade/export/txt/export.php?id=xxxxxx
+
+2. Télécharger le zip du devoir :
+https://studium.umontreal.ca/mod/assign/view.php?id=xxxxxxx&action=downloadall
--- prepare.sh
+++ prepare.sh
@@ -7,12 +7,9 @@ ASSIGNMENT="${1/.zip/}"
 # Extract
 unzip "$1" -d "$ASSIGNMENT" && rm "$ASSIGNMENT.zip"
 
-STUDENTS=`cat students.csv | tr -d \'\" | awk -F, '{ print $3, $1, $2 }'`
+STUDENTS=`cat *.csv | tr -d \'\" | awk -F, '{ print $3, $1, $2 }'`
 for dir in "$ASSIGNMENT"/*
 do
-    # Copy grading file
-    cp grade.md "$dir"
-
     # Rename directory to ID number
     STUDENT_NAME=`basename "${dir/_*/}"` # XXX
     STUDENT_ID=`grep "$STUDENT_NAME" <<< "$STUDENTS" | awk '{ print $1 }'` # XXX
