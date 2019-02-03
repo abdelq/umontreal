830fc350fca4efa26744ce22f01b420904edda59
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-02-03 17:40:50 -0500

    unzip and unrar assignments

--- prepare.sh
+++ prepare.sh
@@ -10,7 +10,7 @@ fi
 ASSIGNMENT="${1/.zip/}"
 
 # Extract
-unzip "$1" -d "$ASSIGNMENT" && rm "$ASSIGNMENT.zip"
+unzip "$1" -d "$ASSIGNMENT" # && rm "$ASSIGNMENT.zip"
 
 STUDENTS=`cat *.csv | tr -d \'\" | awk -F, '{ print $3, $1, $2 }'`
 for dir in "$ASSIGNMENT"/*
@@ -20,3 +20,6 @@ do
     STUDENT_ID=`grep "$STUDENT_NAME" <<< "$STUDENTS" | awk '{ print $1 }'` # XXX
     mv "$dir" "$ASSIGNMENT/$STUDENT_ID"
 done
+
+find "$ASSIGNMENT" -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`" "$filename" && rm "$filename"; done;
+find "$ASSIGNMENT" -name "*.rar" | while read filename; do unrar e "$filename" "`dirname "$filename"`" && rm "$filename"; done;
